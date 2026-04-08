e725bff63461a0bd0168ffe63979d5024c7d699c
uobinnao <uobinnao@gmail.com>
2026-04-08 13:01:49 -0400

    test: allow preview env and smoke-test it locally

--- app/settings.py
+++ app/settings.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
 
     app_name: str = "myapi"
     app_version: str = "0.1.0"
-    app_env: Literal["dev", "staging", "prod"] = "dev"
+    app_env: Literal["dev", "preview","staging", "prod"] = "dev"
 
     usda_base_url: str = "https://api.nal.usda.gov/fdc/v1"
     usda_api_key: str | None = None
@@ -40,33 +40,3 @@ class Settings(BaseSettings):
 def get_settings() -> Settings:
     return Settings()
 
-
-# from functools import lru_cache
-
-# from pydantic import Field
-# from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-# class Settings(BaseSettings):
-#     app_name: str = "Food API"
-#     app_version: str = "1.0.0"
-#     app_env: str = Field(default="development", alias="APP_ENV")
-#     app_port: int = Field(default=8000, alias="APP_PORT")
-
-#     usda_api_key: str | None = Field(default=None, alias="USDA_API_KEY")
-#     usda_base_url: str = "https://api.nal.usda.gov/fdc/v1"
-
-#     rate_limit_window_seconds: int = 15 * 60
-#     rate_limit_max_requests: int = 100
-
-#     model_config = SettingsConfigDict(
-#         env_file=".env",
-#         env_file_encoding="utf-8",
-#         case_sensitive=False,
-#         extra="ignore",
-#     )
-
-
-# @lru_cache
-# def get_settings() -> Settings:
-#     return Settings()
